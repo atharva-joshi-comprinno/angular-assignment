9436dd4b9dbb724555af1515663834fba043e748
Atharva Joshi <atharva.joshi@comprinno.net>
2025-12-16 18:45:37 +0530

    Added theme-toggle

--- pokemon-trainer-app/tailwind.config.js
+++ pokemon-trainer-app/tailwind.config.js
@@ -7,4 +7,17 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    'dark:bg-gray-700',
+    'dark:bg-gray-800', 
+    'dark:bg-gray-900',
+    'dark:bg-gray-600',
+    'dark:text-white',
+    'dark:text-gray-300',
+    'dark:border-gray-600',
+    'dark:hover:bg-gray-600',
+    'dark:hover:bg-purple-800',
+    'dark:hover:text-white',
+    'dark:bg-red-900'
+  ]
 }
